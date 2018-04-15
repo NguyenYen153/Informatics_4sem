1ea3374d9e95cac8d1ccc2ff8deeeb06d0c4205b
Vlad <vlad.miryaha@gmail.com>
2018-04-16 01:56:51 +0300

    fixit

--- vector2.h
+++ vector2.h
@@ -8,24 +8,37 @@ public:
 	
 	vector2();
 	vector2(float a, float b);
+	
+	// fixit: деструктор и конструктор копирования в данном случае не нужны, т.к. компилятор сгенерирует сам ровно тот же код
 	vector2(const vector2& other);
 	~vector2();
+	
 	friend std::ostream& operator<<(std::ostream& stream, const vector2& v);
 	friend std::istream& operator>>(std::istream& stream, vector2& v);
 	vector2 operator*(float k) const;
 	friend vector2 operator*(float k, const vector2& v);
 	vector2& operator+=(const vector2&other);
 	vector2 operator+(const vector2&other)const;
+	
+	// fixit: оператор присваивания тоже не нужно самим писать в данном случае. 
 	const vector2& operator=(const vector2&other);
+	
 	vector2& operator-=(const vector2&other);
 	vector2 operator-(const vector2&orther)const;
 	float operator*(const vector2&other)const;//scalar
+	
+	// вроде векторное произведение - тоже полезная штука :) можно было бы и оставить operator^
 	//float operator^(const vector2&other)const;// 
+	
+	// неясно чем отличается norm от normal
 	vector2& norm();
 	vector2& normal();
+	
 	float len() const;
 	float squareLen() const;
 	vector2 operator-()const;//unary_minus
+	
+	// fixit: rotate должен вернуть vector2&
 	vector2 rotate(float angle);// change the components of the vector x and y 
 								//so that they correspond to the rotated angle vector
 	vector2 getRotate(float angle) const;//return the rotated vector
